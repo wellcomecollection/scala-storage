802f9b7f0fa55a96a92cffee8f4951e570ea0302
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-11 14:38:43 +0100

    Satisfy the imports

--- scripts/config.py
+++ scripts/config.py
@@ -1,8 +1,5 @@
 # -*- encoding: utf-8
 
-from hypothesistooling import modified_files
-
-
 REPO_NAME = 'scala-storage'
 
 TRAVIS_KEY = os.environ['encrypted_12c8071d2874_key']
@@ -10,6 +7,8 @@ TRAVIS_IV = os.environ['encrypted_12c8071d2874_iv']
 
 
 def has_source_changes(version=None):
+    from hypothesistooling import modified_files
+
     if version is None:
         version = latest_version()
 
