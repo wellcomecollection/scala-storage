bb18f8167c33769e7a07ca40f2382d15d8a1c883
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-12 17:36:25 +0100

    We already have Git config from .travis.yml

--- scripts/run_autoformat.py
+++ scripts/run_autoformat.py
@@ -26,23 +26,14 @@ if __name__ == '__main__':
     if subprocess.call(['git', 'diff', '--exit-code']):
         print('*** There were changes from formatting, creating a commit')
 
-        git('config', 'user.name', 'Travis CI on behalf of Wellcome')
-        git('config', 'user.email', 'wellcomedigitalplatform@wellcome.ac.uk')
-        git('config', 'core.sshCommand', 'ssh -i secrets/id_rsa')
-
-        git(
-            'remote', 'add', 'ssh-origin',
-            'git@github.com:wellcometrust/%s.git' % REPO_NAME
-        )
-
         # We checkout the branch before we add the commit, so we don't
         # include the merge commit that Travis makes.
-        git('fetch', 'ssh-origin')
+        git('fetch', 'origin')
         git('checkout', branch_name())
 
         git('add', '--verbose', '--all')
         git('commit', '-m', 'Apply auto-formatting rules')
-        git('push', 'ssh-origin', 'HEAD:%s' % branch_name())
+        git('push', 'origin', 'HEAD:%s' % branch_name())
 
         # We exit here to fail the build, so Travis will skip to the next
         # build, which includes the autoformat commit.
