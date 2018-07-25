74ed7c759b3e7c10d53695bc292fba042b106590
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-25 17:01:14 +0100

    --update, oops

--- sbt_release_tooling.py
+++ sbt_release_tooling.py
@@ -361,7 +361,7 @@ def autoformat():
         git('fetch', 'ssh-origin')
         git('checkout', branch_name())
 
-        git('add', '--verbose', '--updated')
+        git('add', '--verbose', '--update')
         git('commit', '-m', 'Apply auto-formatting rules')
         git('push', 'ssh-origin', 'HEAD:%s' % branch_name())
 
