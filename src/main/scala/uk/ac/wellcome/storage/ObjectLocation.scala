a3cb33b09be03c54e2e0a6f749eaf5ea3e140c24
mklander <mklanderyou@gmail.com>
2018-08-30 12:01:36 +0100

    progress monitor flow

--- src/main/scala/uk/ac/wellcome/storage/ObjectLocation.scala
+++ src/main/scala/uk/ac/wellcome/storage/ObjectLocation.scala
@@ -1,3 +1,5 @@
 package uk.ac.wellcome.storage
 
-case class ObjectLocation(namespace: String, key: String)
+case class ObjectLocation(namespace: String, key: String) {
+  override def toString = s"$namespace/$key"
+}
