f9012de785376d0828bb95d9216fb2f6a814da96
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-14 17:51:11 +0000

    Remove the two deprecated methods

--- src/test/scala/uk/ac/wellcome/storage/fixtures/LocalVersionedHybridStore.scala
+++ src/test/scala/uk/ac/wellcome/storage/fixtures/LocalVersionedHybridStore.scala
@@ -50,11 +50,6 @@ trait LocalVersionedHybridStore
     testWith(store)
   }
 
-  @deprecated("Use the method without the 'bucket' parameter", "after v2.6.0")
-  def assertStored[T](bucket: Bucket, table: Table, id: String, record: T)(
-    implicit encoder: Encoder[T]): Assertion =
-    assertStored(table = table, id = id, record = record)
-
   def assertStored[T](table: Table, id: String, record: T)(
     implicit encoder: Encoder[T]): Assertion =
     assertJsonStringsAreEqual(
@@ -73,14 +68,6 @@ trait LocalVersionedHybridStore
 
     getContentFromS3(hybridRecord.location)
   }
-
-  @deprecated("Use the method without the 'bucket' parameter", "after v2.6.0")
-  def assertStoredCorrectly[T](expectedHybridRecord: HybridRecord, expectedContents: T, bucket: Bucket, table: Table): Assertion =
-    assertStoredCorrectly(
-      expectedHybridRecord = expectedHybridRecord,
-      expectedContents = expectedContents,
-      table = table
-    )
 
   def assertStoredCorrectly[T](expectedHybridRecord: HybridRecord, expectedContents: T, table: Table): Assertion = {
     val hybridRecord = getHybridRecord(table, expectedHybridRecord.id)
