206cf210806546d045d5c85df372748c3ff3109b
Alex Chan <alex@alexwlchan.net>
2018-12-02 22:09:38 +0000

    Add a failing test for storing instances of URI

--- src/test/scala/uk/ac/wellcome/storage/dynamo/DynamoFormatTest.scala
+++ src/test/scala/uk/ac/wellcome/storage/dynamo/DynamoFormatTest.scala
@@ -1,5 +1,6 @@
 package uk.ac.wellcome.storage.dynamo
 
+import java.net.URI
 import java.time.Instant
 
 import com.gu.scanamo.{DynamoFormat, Scanamo}
@@ -12,12 +13,20 @@ class DynamoFormatTest extends FunSpec with LocalDynamoDbVersioned with Matchers
     val id: String
   }
 
-  it("allows storing and retrieving an instance of an Instant") {
+  it("allows storing and retrieving an instance of Instant") {
     case class Timestamp(id: String, datetime: Instant) extends Identifiable
 
     val record = Timestamp(id = "1", datetime = Instant.now)
 
     assertCanStoreAndRetrieve[Timestamp](record = record)
+  }
+
+  it("allows storing and retrieving an instance of URI") {
+    case class Webpage(id: String, uri: URI) extends Identifiable
+
+    val record = Webpage(id = "1", uri = new URI("https://example.org/"))
+
+    assertCanStoreAndRetrieve[Webpage](record = record)
   }
 
   private def assertCanStoreAndRetrieve[T <: Identifiable](record: T)(
