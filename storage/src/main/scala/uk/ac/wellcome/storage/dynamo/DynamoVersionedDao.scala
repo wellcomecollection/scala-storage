0cbe7c3f6e205bdba32f316acce387e031a5bb71
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-17 13:40:04 +0100

    If we're bumping the major version, we can ditch all this code

--- storage/src/main/scala/uk/ac/wellcome/storage/dynamo/DynamoVersionedDao.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/dynamo/DynamoVersionedDao.scala
@@ -1,36 +1,13 @@
 package uk.ac.wellcome.storage.dynamo
 
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputExceededException
 import grizzled.slf4j.Logging
 import uk.ac.wellcome.storage.VersionedDao
 import uk.ac.wellcome.storage.type_classes.{VersionGetter, VersionUpdater}
 
-import scala.concurrent.{ExecutionContext, Future}
-
 class DynamoVersionedDao[Ident, T](
   val underlying: DynamoConditionalUpdateDao[Ident, T]
 )(implicit
-  ec: ExecutionContext,
   val versionGetter: VersionGetter[T],
   val versionUpdater: VersionUpdater[T])
     extends Logging
-    with VersionedDao[Ident, T] {
-
-  @deprecated("Use put() instead of updateRecord()", since = "2019-05-10")
-  def updateRecord[R](record: T): Future[T] =
-    Future
-      .fromTry { put(record) }
-      .recover {
-        case t: ProvisionedThroughputExceededException =>
-          throw DynamoNonFatalError(t)
-      }
-
-  @deprecated("Use get() instead of getRecord()", since = "2019-05-10")
-  def getRecord[R](id: Ident): Future[Option[T]] =
-    Future
-      .fromTry { get(id) }
-      .recover {
-        case t: ProvisionedThroughputExceededException =>
-          throw DynamoNonFatalError(t)
-      }
-}
+    with VersionedDao[Ident, T]
