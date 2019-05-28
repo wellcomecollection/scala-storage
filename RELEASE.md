ae53531e3f9d210e5b900d93333efe5fab274d3e
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-28 10:41:15 +0100

    Add DynamoFormat for VHSEntry

--- RELEASE.md
+++ RELEASE.md
@@ -1,3 +1,7 @@
 RELEASE_TYPE: patch
 
-In the VersionedHybridStore, when updating, don't write to the ObjectStore unless the underlying object has actually changed.
+*   In the VersionedHybridStore, when updating, don't write to the ObjectStore unless the underlying object has actually changed.
+
+*   Make it easier to access underlying entries in MemoryVersionedDao
+
+*   Add a way to delete objects from MemoryObjectStore
---
+++ storage/src/main/scala/uk/ac/wellcome/storage/vhs/vhs.scala
@@ -0,0 +1,41 @@
+package uk.ac.wellcome.storage
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import com.gu.scanamo.DynamoFormat
+import com.gu.scanamo.error.DynamoReadError
+import uk.ac.wellcome.storage.dynamo.UpdateExpressionGenerator
+
+package object vhs {
+  implicit val vhsEntryFormat: DynamoFormat[Entry[String, EmptyMetadata]] = new DynamoFormat[Entry[String, EmptyMetadata]] {
+    override def read(av: AttributeValue): Either[DynamoReadError, Entry[String, EmptyMetadata]] =
+      DynamoFormat[PlainEntry[String]].read(av).map {
+        plainEntry =>
+          Entry(
+            id = plainEntry.id,
+            version = plainEntry.version,
+            location = plainEntry.location,
+            metadata = EmptyMetadata()
+          )
+      }
+
+    override def write(entry: Entry[String, EmptyMetadata]): AttributeValue = {
+      val plainEntry = PlainEntry[String](
+        id = entry.id,
+        version = entry.version,
+        location = entry.location
+      )
+
+      DynamoFormat[PlainEntry[String]].write(plainEntry)
+    }
+  }
+
+  implicit val updateExpressionGenerator: UpdateExpressionGenerator[Entry[String, EmptyMetadata]] =
+    (entry: Entry[String, EmptyMetadata]) =>
+      UpdateExpressionGenerator[PlainEntry[String]].generateUpdateExpression(
+        PlainEntry[String](
+          id = entry.id,
+          version = entry.version,
+          location = entry.location
+        )
+      )
+}
