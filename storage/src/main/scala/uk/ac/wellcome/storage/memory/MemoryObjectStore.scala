5b600a2ae0e9c0b4d9d448c3406e474e5f95cc11
Alex Chan <alex@alexwlchan.net>
2019-05-31 15:18:29 +0100

    Type the backend of MemoryObjectStore as a memory backend

--- storage/src/main/scala/uk/ac/wellcome/storage/memory/MemoryObjectStore.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/memory/MemoryObjectStore.scala
@@ -1,11 +1,11 @@
 package uk.ac.wellcome.storage.memory
 
 import uk.ac.wellcome.storage.streaming.Codec
-import uk.ac.wellcome.storage.{ObjectStore, StorageBackend}
+import uk.ac.wellcome.storage.ObjectStore
 
 class MemoryObjectStore[T](
   implicit val codec: Codec[T]
 ) extends ObjectStore[T] {
-  override implicit val storageBackend: StorageBackend =
+  override implicit val storageBackend: MemoryStorageBackend =
     new MemoryStorageBackend()
 }
