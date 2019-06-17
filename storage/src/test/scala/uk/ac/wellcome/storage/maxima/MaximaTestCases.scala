9926b87a4d7f0476c74339ffa4be3cfa013380e3
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-17 14:28:49 +0100

    Add some tests for MemoryMaxima
    Co-authored-by: Robert Kenny <r.kenny@wellcome.ac.uk>

---
+++ storage/src/test/scala/uk/ac/wellcome/storage/maxima/MaximaTestCases.scala
@@ -0,0 +1,61 @@
+package uk.ac.wellcome.storage.maxima
+
+import org.scalatest.{EitherValues, FunSpec, Matchers}
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.storage.{IdentityKey, NoMaximaValueError, Version}
+import uk.ac.wellcome.storage.generators.{Record, RecordGenerators}
+
+trait MaximaTestCases extends FunSpec with Matchers with RecordGenerators with EitherValues {
+  type MaximaStub = Maxima[IdentityKey, Int]
+
+  def withMaxima[R](initialEntries: Map[Version[IdentityKey, Int], Record])(
+    testWith: TestWith[MaximaStub, R]): R
+
+  it("finds the maximum value with one matching entry") {
+    val id = createIdentityKey
+
+    val initialEntries = Map(
+      Version(id, 1) -> createRecord
+    )
+
+    withMaxima(initialEntries) { maxima =>
+      maxima.max(id).right.value shouldBe 1
+    }
+  }
+
+  it("finds the maximum value with multiple matching entries") {
+    val id = createIdentityKey
+
+    val initialEntries = Map(
+      Version(id, 1) -> createRecord,
+      Version(id, 2) -> createRecord,
+      Version(id, 3) -> createRecord,
+      Version(id, 5) -> createRecord,
+    )
+
+    withMaxima(initialEntries) { maxima =>
+      maxima.max(id).right.value shouldBe 5
+    }
+  }
+
+  it("only looks at the identifier in question") {
+    val id = createIdentityKey
+
+    val initialEntries = Map(
+      Version(id, 1) -> createRecord,
+      Version(id, 2) -> createRecord,
+      Version(id, 3) -> createRecord,
+      Version(createIdentityKey, 5) -> createRecord,
+    )
+
+    withMaxima(initialEntries) { maxima =>
+      maxima.max(id).right.value shouldBe 3
+    }
+  }
+
+  it("errors if there are no matching entries") {
+    withMaxima(initialEntries = Map.empty) { maxima =>
+      maxima.max(createIdentityKey).left.value shouldBe a[NoMaximaValueError]
+    }
+  }
+}
---
+++ storage/src/test/scala/uk/ac/wellcome/storage/maxima/memory/MemoryMaximaTest.scala
@@ -0,0 +1,17 @@
+package uk.ac.wellcome.storage.maxima.memory
+
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.storage.{IdentityKey, Version}
+import uk.ac.wellcome.storage.generators.Record
+import uk.ac.wellcome.storage.maxima.{MaximaTestCases}
+
+class MemoryMaximaTest extends MaximaTestCases {
+  override def withMaxima[R](initialEntries: Map[Version[IdentityKey, Int], Record])(testWith: TestWith[MaximaStub, R]): R = {
+    val maxima = new MemoryMaxima[IdentityKey, Record] {
+      override var entries: Map[Version[IdentityKey, Int], Record] = initialEntries
+    }
+
+    testWith(maxima)
+  }
+}
+
