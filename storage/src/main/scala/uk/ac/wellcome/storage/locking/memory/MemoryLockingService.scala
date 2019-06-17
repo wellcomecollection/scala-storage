3e4d6b5e604a77d86e85b1d568f461188c07aab2
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-06-17 13:10:21 +0000

    Apply auto-formatting rules

--- storage/src/main/scala/uk/ac/wellcome/storage/locking/memory/MemoryLockingService.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/locking/memory/MemoryLockingService.scala
@@ -6,8 +6,9 @@ import uk.ac.wellcome.storage.locking.{LockDao, LockingService}
 
 import scala.language.higherKinds
 
-class MemoryLockingService[Out, OutMonad[_]](implicit val lockDao: MemoryLockDao[String, UUID])
-  extends LockingService[Out, OutMonad, LockDao[String, UUID]] {
+class MemoryLockingService[Out, OutMonad[_]](
+  implicit val lockDao: MemoryLockDao[String, UUID])
+    extends LockingService[Out, OutMonad, LockDao[String, UUID]] {
   override protected def createContextId(): lockDao.ContextId =
     UUID.randomUUID()
 }
