a1da976a307d07846fb120ad1240515b1bd6d7f4
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-18 13:51:27 +0100

    Separate out the namespace fixtures for a Bucket
    Co-authored-by: Robert Kenny <r.kenny@wellcome.ac.uk>

--- storage/src/test/scala/uk/ac/wellcome/storage/store/fixtures/NamespaceFixtures.scala
+++ storage/src/test/scala/uk/ac/wellcome/storage/store/fixtures/NamespaceFixtures.scala
@@ -1,6 +1,9 @@
 package uk.ac.wellcome.storage.store.fixtures
 
 import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.storage.ObjectLocation
+import uk.ac.wellcome.storage.fixtures.S3Fixtures
+import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
 import uk.ac.wellcome.storage.generators.RandomThings
 
 trait NamespaceFixtures[Ident, Namespace] {
@@ -15,4 +18,17 @@ trait StringNamespaceFixtures extends NamespaceFixtures[String, String] with Ran
 
   override def createId(implicit namespace: String): String =
     s"$namespace/$randomAlphanumeric"
+}
+
+trait BucketNamespaceFixtures extends NamespaceFixtures[ObjectLocation, Bucket] with S3Fixtures {
+  override def withNamespace[R](testWith: TestWith[Bucket, R]): R =
+    withLocalS3Bucket { bucket =>
+      testWith(bucket)
+    }
+
+  override def createId(implicit namespace: Bucket): ObjectLocation =
+    ObjectLocation(
+      namespace = namespace.name,
+      key = randomAlphanumeric
+    )
 }
--- storage/src/test/scala/uk/ac/wellcome/storage/store/s3/S3StreamStoreTest.scala
+++ storage/src/test/scala/uk/ac/wellcome/storage/store/s3/S3StreamStoreTest.scala
@@ -2,12 +2,12 @@ package uk.ac.wellcome.storage.store.s3
 
 import uk.ac.wellcome.fixtures.TestWith
 import uk.ac.wellcome.storage.ObjectLocation
-import uk.ac.wellcome.storage.fixtures.S3Fixtures
 import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
 import uk.ac.wellcome.storage.store.StreamStoreTestCases
+import uk.ac.wellcome.storage.store.fixtures.BucketNamespaceFixtures
 import uk.ac.wellcome.storage.streaming.InputStreamWithLengthAndMetadata
 
-class S3StreamStoreTest extends StreamStoreTestCases[ObjectLocation, Bucket, Unit] with S3Fixtures {
+class S3StreamStoreTest extends StreamStoreTestCases[ObjectLocation, Bucket, Unit] with BucketNamespaceFixtures {
   override def withStoreImpl[R](
     storeContext: Unit,
     initialEntries: Map[ObjectLocation, InputStreamWithLengthAndMetadata])(
@@ -21,15 +21,4 @@ class S3StreamStoreTest extends StreamStoreTestCases[ObjectLocation, Bucket, Uni
 
   override def withStoreContext[R](testWith: TestWith[Unit, R]): R =
     testWith(())
-
-  override def withNamespace[R](testWith: TestWith[Bucket, R]): R =
-    withLocalS3Bucket { bucket =>
-      testWith(bucket)
-    }
-
-  override def createId(implicit namespace: Bucket): ObjectLocation =
-    ObjectLocation(
-      namespace = namespace.name,
-      key = randomAlphanumeric
-    )
 }
