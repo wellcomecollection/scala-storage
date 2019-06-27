fb410c977a6a47ab6c1a0a2a2807f3e8161998d8
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-06-27 09:12:47 +0000

    Apply auto-formatting rules

--- storage/src/main/scala/uk/ac/wellcome/storage/store/dynamo/DynamoHybridStoreWithMaxima.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/store/dynamo/DynamoHybridStoreWithMaxima.scala
@@ -3,10 +3,14 @@ package uk.ac.wellcome.storage.store.dynamo
 import java.util.UUID
 
 import uk.ac.wellcome.storage.{ObjectLocation, ObjectLocationPrefix, Version}
-import uk.ac.wellcome.storage.store.{HybridIndexedStoreEntry, HybridStoreWithMaxima}
+import uk.ac.wellcome.storage.store.{
+  HybridIndexedStoreEntry,
+  HybridStoreWithMaxima
+}
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 
-class DynamoHybridStoreWithMaxima[Id, V, T, Metadata](prefix: ObjectLocationPrefix)(
+class DynamoHybridStoreWithMaxima[Id, V, T, Metadata](
+  prefix: ObjectLocationPrefix)(
   implicit
   val indexedStore: DynamoHashRangeStore[
     Id,
@@ -15,7 +19,9 @@ class DynamoHybridStoreWithMaxima[Id, V, T, Metadata](prefix: ObjectLocationPref
   val typedStore: S3TypedStore[T]
 ) extends HybridStoreWithMaxima[Id, V, ObjectLocation, T, Metadata] {
 
-  override protected def createTypeStoreId(
-    id: Version[Id, V]): ObjectLocation =
-    prefix.asLocation(id.id.toString, id.version.toString, UUID.randomUUID().toString)
+  override protected def createTypeStoreId(id: Version[Id, V]): ObjectLocation =
+    prefix.asLocation(
+      id.id.toString,
+      id.version.toString,
+      UUID.randomUUID().toString)
 }
--- storage/src/main/scala/uk/ac/wellcome/storage/store/dynamo/DynamoVersionedHybridStore.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/store/dynamo/DynamoVersionedHybridStore.scala
@@ -4,5 +4,6 @@ import uk.ac.wellcome.storage.ObjectLocation
 import uk.ac.wellcome.storage.store.VersionedHybridStore
 
 class DynamoVersionedHybridStore[Id, V, T, Metadata](
-  store: DynamoHybridStoreWithMaxima[Id, V, T, Metadata])(implicit N: Numeric[V])
+  store: DynamoHybridStoreWithMaxima[Id, V, T, Metadata])(
+  implicit N: Numeric[V])
     extends VersionedHybridStore[Id, V, ObjectLocation, T, Metadata](store)
