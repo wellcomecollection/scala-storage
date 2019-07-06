be36c21de05e399355b4aa3a1d7f7d3e4e6d37ff
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-06 16:31:54 +0100

    Add an ObjectLocation trait

--- storage/src/main/scala/uk/ac/wellcome/storage/ObjectLocation.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/ObjectLocation.scala
@@ -2,6 +2,53 @@ package uk.ac.wellcome.storage
 
 import java.nio.file.Paths
 
+trait BetterObjectLocation {
+  val namespace: String
+  val path: String
+
+  override def toString = s"$namespace/$path"
+
+  def join(parts: String*): _ <: BetterObjectLocation
+
+  def asPrefix: _ <: BetterObjectLocationPrefix
+}
+
+trait BetterObjectLocationPrefix {
+  val namespace: String
+  val pathPrefix: String
+
+  def asLocation(parts: String*): _ <: BetterObjectLocation
+}
+
+case class S3ObjectLocation(bucket: String, key: String) extends BetterObjectLocation {
+  val namespace: String = bucket
+  val path: String = key
+
+  override def toString = s"s3://$bucket/$key"
+
+  override def join(parts: String*): S3ObjectLocation =
+    this.copy(
+      key = Paths.get(this.key, parts: _*).toString
+    )
+
+  override def asPrefix: S3ObjectLocationPrefix =
+    S3ObjectLocationPrefix(
+      bucket = bucket,
+      keyPrefix = key
+    )
+}
+
+case class S3ObjectLocationPrefix(bucket: String, keyPrefix: String) extends BetterObjectLocationPrefix {
+  override val namespace: String = bucket
+  override val pathPrefix: String = keyPrefix
+
+  override def asLocation(parts: String*): S3ObjectLocation =
+    S3ObjectLocation(
+      bucket = bucket,
+      key = Paths.get(this.keyPrefix, parts: _*).toString
+    )
+}
+
 case class ObjectLocation(namespace: String, path: String) {
   override def toString = s"$namespace/$path"
 
@@ -15,6 +62,8 @@ case class ObjectLocation(namespace: String, path: String) {
       path = path
     )
 }
+
+
 
 case class ObjectLocationPrefix(namespace: String, path: String) {
   def asLocation(parts: String*): ObjectLocation =
