c88e77271d99d03e4d4320e547b7f58b5a8532a6
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-07 09:38:00 +0100

    Tidy up S3ObjectLocationPrefix

--- storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ObjectLocationPrefix.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ObjectLocationPrefix.scala
@@ -1,16 +1,13 @@
 package uk.ac.wellcome.storage.s3
 
-import java.nio.file.Paths
-
 import uk.ac.wellcome.storage.NamespacedPath
 
 case class S3ObjectLocationPrefix(bucket: String, keyPrefix: String) extends NamespacedPath {
   override val namespace: String = bucket
   override val path: String = keyPrefix
 
+  override def toString = s"s3://$bucket/$key"
+
   def asLocation(parts: String*): S3ObjectLocation =
-    S3ObjectLocation(
-      bucket = bucket,
-      key = Paths.get(this.keyPrefix, parts: _*).toString
-    )
+    S3ObjectLocation(bucket, key = keyPrefix).join(parts: _*)
 }
