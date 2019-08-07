81bc77ec90b99d4445d50e12b92e7d6c6565a4f9
Alex Chan <alex@alexwlchan.net>
2019-08-07 09:53:04 +0100

    Check for the presence of the destination object first
    We don't need to make a GetObject call against the source object if
    there's no destination object to compare to.  In the case where we're
    replicating something entirely fresh, this saves a lot of S3 calls.
    
    In quick testing, it takes the time to replicate 250 objects with S3
    from 2-3s to ~1.5s.

--- storage/src/main/scala/uk/ac/wellcome/storage/transfer/s3/S3Transfer.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/transfer/s3/S3Transfer.scala
@@ -35,51 +35,39 @@ class S3Transfer(implicit s3Client: AmazonS3) extends Transfer[ObjectLocation] {
         s3Client.getObject(location.namespace, location.path)
       }.map { _.getObjectContent }
 
-    val trySrcStream = getStream(src)
-    val tryDstStream = getStream(dst)
-
-    (trySrcStream, tryDstStream) match {
-      // If the destination object exists and is the same as the source
-      // object, we can skip the copy operation.
-      case (Success(srcStream), Success(dstStream)) =>
-        val result = compare(srcStream, dstStream)
-
-        // Remember to close the streams afterwards, or we might get
-        // errors like
-        //
-        //    Unable to execute HTTP request: Timeout waiting for
-        //    connection from pool
-        //
-        // See: https://github.com/wellcometrust/platform/issues/3600
-        //      https://github.com/aws/aws-sdk-java/issues/269
-        //
-        srcStream.close()
-        dstStream.close()
-
-        result
-
-      case (Success(srcStream), _) =>
-        // We have to abort() rather than close() the connection, or we get
-        // warnings like:
-        //
-        //      Not all bytes were read from the S3ObjectInputStream, aborting HTTP connection.
-        //      This is likely an error and may result in sub-optimal behavior. Request only
-        //      the bytes you need via a ranged GET or drain the input stream after use.
-        //
-        // In our case it isn't an error -- if there's no dst stream to compare to,
-        // we don't need to read the original stream.
-        srcStream.abort()
-
+    getStream(dst) match {
+      // If the destination object doesn't exist, we can go ahead and
+      // start the transfer.
+      case Failure(_) =>
         runTransfer(src, dst)
 
-      case (Failure(err), Success(dstStream)) =>
-        // As above, we need to abort the input stream so we don't leave streams
-        // open or get warnings from the SDK.
-        dstStream.abort()
-        Left(TransferSourceFailure(src, dst, err))
+      case Success(dstStream) =>
+        getStream(src) match {
+          // If both the source and the destination exist, we can skip
+          // the copy operation.
+          case Success(srcStream) =>
+            val result = compare(srcStream, dstStream)
 
-      case (Failure(err), _) =>
-        Left(TransferSourceFailure(src, dst, err))
+            // Remember to close the streams afterwards, or we might get
+            // errors like
+            //
+            //    Unable to execute HTTP request: Timeout waiting for
+            //    connection from pool
+            //
+            // See: https://github.com/wellcometrust/platform/issues/3600
+            //      https://github.com/aws/aws-sdk-java/issues/269
+            //
+            srcStream.close()
+            dstStream.close()
+
+            result
+
+          case Failure(err) =>
+            // As above, we need to abort the input stream so we don't leave streams
+            // open or get warnings from the SDK.
+            dstStream.abort()
+            Left(TransferSourceFailure(src, dst, err))
+        }
     }
   }
 
