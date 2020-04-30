0ad8047e0a8050cd30afb636f4e6b7dde308dc7f
Alex Chan <alex@alexwlchan.net>
2020-04-30 13:43:32 +0100

    Add different methods to Transfer for overwrite or not

--- storage/src/main/scala/uk/ac/wellcome/storage/transfer/Transfer.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/transfer/Transfer.scala
@@ -2,5 +2,15 @@ package uk.ac.wellcome.storage.transfer
 
 trait Transfer[Location] {
   def transfer(src: Location,
-               dst: Location): Either[TransferFailure, TransferSuccess]
+               dst: Location,
+               checkForExisting: Boolean = true): Either[TransferFailure, TransferSuccess] =
+    if (checkForExisting) {
+      transferWithCheckForExisting(src, dst)
+    } else {
+      transferWithOverwrites(src, dst)
+    }
+
+  protected def transferWithCheckForExisting(src: Location, dst: Location): Either[TransferFailure, TransferSuccess]
+
+  protected def transferWithOverwrites(src: Location, dst: Location): Either[TransferFailure, TransferSuccess]
 }
