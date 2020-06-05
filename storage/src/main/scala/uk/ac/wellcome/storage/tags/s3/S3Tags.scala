8397aba2cce40f22a3f43275ac845bdff58f0371
Alex Chan <alex@alexwlchan.net>
2020-06-05 14:32:20 +0100

    Add the put() method to S3ObjectTagging

--- storage/src/main/scala/uk/ac/wellcome/storage/tags/s3/S3Tags.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/tags/s3/S3Tags.scala
@@ -1,12 +1,13 @@
 package uk.ac.wellcome.storage.tags.s3
 
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.{GetObjectTaggingRequest, Tag}
+import com.amazonaws.services.s3.model.{GetObjectTaggingRequest, ObjectTagging, SetObjectTaggingRequest, Tag}
 import uk.ac.wellcome.storage.s3.S3Get
-import uk.ac.wellcome.storage.{ObjectLocation, ReadError, WriteError}
+import uk.ac.wellcome.storage.{ObjectLocation, ReadError, StoreWriteError, WriteError}
 import uk.ac.wellcome.storage.tags.Tags
 
 import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
 
 class S3Tags(maxRetries: Int = 3)(implicit s3Client: AmazonS3) extends Tags[ObjectLocation] {
   override def get(location: ObjectLocation): Either[ReadError, Map[String, String]] =
@@ -25,5 +26,23 @@ class S3Tags(maxRetries: Int = 3)(implicit s3Client: AmazonS3) extends Tags[Obje
         .toMap
     } yield tags
 
-  override protected def put(location: ObjectLocation, tags: Map[String, String]): Either[WriteError, Map[String, String]] = ???
+  override protected def put(location: ObjectLocation, tags: Map[String, String]): Either[WriteError, Map[String, String]] = {
+    val tagSet = tags
+      .map { case (k, v) => new Tag(k, v) }
+      .toSeq
+      .asJava
+
+    Try {
+      s3Client.setObjectTagging(
+        new SetObjectTaggingRequest(
+          location.namespace,
+          location.path,
+          new ObjectTagging(tagSet)
+        )
+      )
+    } match {
+      case Success(_)   => Right(tags)
+      case Failure(err) => Left(StoreWriteError(err))
+    }
+  }
 }
